b2b914f2edaef16d5cead39aac3751b940d5cd7f
izah2587 <is2587@nyu.edu>
2024-10-24 16:15:31 -0400

    commit

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,15 @@ const { Pool } = require('pg');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+require('dotenv').config();
 
 // PostgreSQL Connection Pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'ScheduleMate',
-  password: 'izahaha#567',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // -------------------- API Routes --------------------
